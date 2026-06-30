911cd132347fd946d3de60dbbc9d791cfffffb13
Siddharth Ajith <63159870+siddddd17@users.noreply.github.com>
2026-06-30 15:15:14 +0530

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        // #bruteforce
+        // [1,2,4,6]
+        // tc: o(n^2)
+        // sc: o(1)
+        // product = 1
+        // product * = j 
+        // assign the product to result[i]
+        // return result[i]
+
+        // first loop through the entire array , compute the total product
+        // total product = x
+        // hashmap<integer, integer> index, product ; 
+        // loop 2 : result[i] = product / nums[i]
+        // tc: o(2n ) -> o(n)
+        // sc : o(n)
+
+        int product =1 ; 
+        int zeroCtr = 0 ; 
+        for(int num : nums){
+            if (num == 0){
+                zeroCtr++; 
+                continue; 
+            }
+            product*=num;
+        }
+        if (zeroCtr > 1)
+            return new int[nums.length];
+        
+        int[] result = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
+            if (zeroCtr > 0 ){
+                result[i] = (nums[i] == 0) ? product : 0 ;
+            }
+            else result[i] = product / nums[i];
+        }
+
+        return result; 
+
+
+    }
+}  
